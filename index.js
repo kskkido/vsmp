e3ef5d2247bec9ea67b99e76a156ab1d61144197
Keisuke Kido <kskkido@gmail.com>
2019-09-13 08:32:28 +0900

    create progress reset

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('./utils/promise');
+const { displayFromFile } = require('./modules/epd');
+const { singleFrame } = require('./modules/single-frame');
+const { frame } = require('./config.json');
+
+const relativePath = filePath => (
+  path.join(
+    path.dirname(fs.realpathSync(__filename)),
+    filePath
+  )
+);
+
+const reset = () => (
+  promisify(fs.writeFile)(
+    relativePath('/config.json'),
+    JSON.stringify(({ frame: 0 }), '', ' ')
+  )
+)
+
+const progress = () => (
+  singleFrame({
+    frame,
+    input: relativePath('/assets/caveOfRebirth.mp4'),
+  })
+    .then(displayFromFile)
+    .then(() => promisify(fs.writeFile)(
+      relativePath('/config.json'),
+      JSON.stringify(({ frame: frame + 1 }), '', ' ')
+    ))
+    .catch(e => console.error(e))
+);
+
+module.exports = {
+  reset,
+  progress
+}
